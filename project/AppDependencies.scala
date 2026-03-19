e6c830cf20e8ea08b7c5962073740a9fe947ffad
bolaji <bolaji.abiodon2@digital.hmrc.gov.uk>
2026-03-19 13:16:15 +0000

    Updated dependencies to remove violations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "10.5.0"
+  private val bootstrapPlayVersion = "10.7.0"
   private val playHmrcVersion = "8.3.0"
   private val domainVersion = "13.0.0"
   private val reactiveCircuitBreakerVersion = "6.1.0"
